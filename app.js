66d07b109be2dfca433d57a0ad66caa647894196
MahmoudAliEid <alim46972@gmail.com>
2024-12-21 21:28:53 +0200

    fix formatting of phone number in contact email message

--- app.js
+++ app.js
@@ -51,9 +51,7 @@ app.post("/contact-us", (req, res) => {
         message: "Please fill all the fields",
       });
     }
-    const massageWithPhone = `${massage}, My Phone: ${phone}
-
-      `;
+    const massageWithPhone = `${massage}, My Phone: ${phone}`;
     const info = sendEmail({ name, email, massageWithPhone, subject });
     res.status(200).json({
       success: true,
