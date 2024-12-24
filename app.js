b643f3f0e4e9b9a9e63a3cbf7b2db3f074c2fea5
Mahmoud  Ali <61747217+MahmoudAliEid@users.noreply.github.com>
2024-12-24 05:08:27 +0200

    Update app.js for cors frontend local #2

--- app.js
+++ app.js
@@ -37,13 +37,7 @@ app.options("*", (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.sendStatus(200);
 });
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+
 
 
 // ** Serve static files from the 'images' directory
